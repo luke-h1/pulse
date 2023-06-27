20bf665b93a39a0b77dabaf449285a3b1ebcb09d
luke-h1 <luke.howsam@yahoo.com>
2023-06-27 08:27:11 +0100

    feat(api): get post by id

--- apps/api/src/graphql/post/queries.ts
+++ apps/api/src/graphql/post/queries.ts
@@ -1,4 +1,7 @@
+import { GraphQLError } from 'graphql';
 import builder from '../../builder';
+import db from '../../db';
+import { SearchOrder } from '../user/queries';
 
 export const Post = builder.prismaObject('Post', {
   name: 'Post',
@@ -24,3 +27,53 @@ export const Post = builder.prismaObject('Post', {
     }),
   }),
 });
+
+export const PostsResponse = builder.objectType('PostsResponse', {
+  description: 'PostsResponse',
+  fields: t => ({
+    nextCursor: t.exposeString('nextCursor', {
+      nullable: true,
+    }),
+    prevCursor: t.exposeString('prevCursor', {
+      nullable: true,
+    }),
+    totalCount: t.exposeInt('totalCount'),
+    results: t.expose('results', {
+      type: [Post],
+    }),
+  }),
+});
+
+const PostSearchInput = builder.inputType('PostSearchInput', {
+  description: 'Search posts input',
+  fields: t => ({
+    cursor: t.string({ required: false }),
+    search: t.string(),
+    order: t.field({ type: SearchOrder }),
+    orderBy: t.string(),
+  }),
+});
+
+builder.queryFields(t => ({
+  getPost: t.prismaField({
+    type: Post,
+    description: 'Get post by id', // TODO lh - get by slug instead
+    args: {
+      id: t.arg.id({ required: true }),
+    },
+    resolve: async (query, _, args) => {
+      const post = await db.post.findUnique({
+        ...query,
+        where: {
+          id: args.id.toString(),
+        },
+      });
+
+      if (!post) {
+        throw new GraphQLError('Post not found');
+      }
+
+      return post;
+    },
+  }),
+}));

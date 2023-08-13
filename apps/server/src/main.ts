940ca0d0ea9994fb3a7841c9ea34e7b23ab5814a
luke-h1 <luke.howsam@yahoo.com>
2023-08-13 20:40:32 +0100

    feat(server): migrate to @apollo/server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import http from 'http';
 import express from 'express';
 import session from 'express-session';
-import { ApolloServer } from 'apollo-server-express';
 import compression from 'compression';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
+import { ApolloServer } from '@apollo/server';
+import { json } from 'body-parser';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import redis from './db/redis';
 import buildCookieOptions from './utils/buildCookieOptions';
-import { isProd } from './utils/isProd';
 import createSchema from './utils/createSchema';
 import logger from './utils/logger';
 import config from './utils/config';
@@ -23,6 +26,8 @@ const PROD_ORIGINS = [''];
 
 const main = async () => {
   const app = express();
+  app.set('trust proxy', 1);
+  const httpServer = http.createServer(app);
 
   app.use(compression());
 
@@ -43,35 +48,35 @@ const main = async () => {
     }),
   );
 
-  app.use(
-    cors({
-      origin:
-        process.env.NODE_ENV === 'production' ? PROD_ORIGINS : DEV_ORIGINS,
-      credentials: true,
-    }),
-  );
-
   const apolloServer = new ApolloServer({
-    plugins: [],
-    debug: !!isProd,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     allowBatchedHttpRequests: true,
     schema: await createSchema(),
-    context: ({ req, res }) => ({ req, res, redis }),
   });
 
   await apolloServer.start();
 
-  apolloServer.applyMiddleware({
-    app,
-    cors: false,
-    path: '/api',
-  });
+  app.use(
+    '/api/graphql',
+    cors<cors.CorsRequest>({
+      origin:
+        process.env.NODE_ENV === 'production' ? PROD_ORIGINS : DEV_ORIGINS,
+      credentials: true,
+    }),
+    json({
+      limit: '50mb',
+    }),
+    expressMiddleware(apolloServer, {
+      context: async ({ req, res }) => ({ req, res, redis }),
+    }),
+  );
 
-  app.listen(process.env.PORT, () => {
-    logger.info(
-      `API started on http://localhost:${process.env.PORT}/api/graphql`,
-    );
-  });
+  await new Promise<void>(resolve =>
+    httpServer.listen({ port: process.env.PORT }, resolve),
+  );
+  logger.info(
+    `Server listening on http://localhost:${process.env.PORT}/api/graphql`,
+  );
 };
 
 main().catch(e => logger.error(e));

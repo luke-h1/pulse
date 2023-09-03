50b195a3a44ecff5213c64fb1320d37b403a8263
luke-h1 <luke.howsam@yahoo.com>
2023-09-03 17:43:37 +0100

    fix(server): fix tests

---
+++ apps/frontend/src/pages/posts/[id].tsx
@@ -0,0 +1,72 @@
+import { NextPage } from 'next';
+import { VStack, Heading, HStack, Text, Divider } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '@frontend/utils/createUrqlClient';
+import { usePostQuery } from '@graphql-hooks/generated';
+import FormattedDate from '@common/components/FormattedDate';
+import ScrollToTop from '@frontend/components/ScrollToTop';
+import useGetIntId from '@common/hooks/useGetIntId';
+import { isServer } from '@common/hooks';
+
+const PostSlugPage: NextPage = () => {
+  const router = useRouter();
+
+  const [{ data, fetching }] = usePostQuery({
+    pause: isServer,
+    variables: {
+      id: router.query.id as string,
+    },
+  });
+
+  return (
+    !fetching && (
+      <>
+        <VStack position="relative" alignItems="stretch" w="full" spacing={8}>
+          <VStack alignItems="flex-start" spacing={3}>
+            <Heading as="h1" size="lg">
+              {data?.post?.title}
+            </Heading>
+            <HStack
+              divider={
+                <Text mx={2} color="gray.500">
+                  •
+                </Text>
+              }
+            >
+              <Text color="gray.500" fontSize="sm">
+                <FormattedDate>{data?.post?.createdAt}</FormattedDate>
+              </Text>
+              {/* needs BE functionality */}
+              {/* <HStack>
+              {!views && <Spinner color='gray.500' size='xs' />}
+              {views && (
+                <Text color='gray.500' fontSize='sm'>
+                  {views} views
+                </Text>
+              )}
+            </HStack> */}
+              <Text color="gray.500" fontSize="sm">
+                {data?.post?.readingTime}
+              </Text>
+            </HStack>
+          </VStack>
+          {/* editorjs output here */}
+          <Divider />
+          {/* needs BE functionality */}
+          {/* {!fetching && (
+          <HStack alignItems="center" justifyContent="center">
+            <LikeButton
+              onLike={incrementLikes}
+              likes={likes}
+              userLikes={userLikes}
+            />
+          </HStack>
+        )} */}
+        </VStack>
+        <ScrollToTop />
+      </>
+    )
+  );
+};
+export default withUrqlClient(createUrqlClient, { ssr: true })(PostSlugPage);

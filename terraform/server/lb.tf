c004ffbe2af22f51b4836588a37cac8c2672186f
luke-h1 <luke.howsam@yahoo.com>
2023-10-03 14:10:16 +0100

    chore: fix tf

--- terraform/server/lb.tf
+++ terraform/server/lb.tf
@@ -24,12 +24,8 @@ resource "aws_lb_listener" "server" {
   port              = 80
   protocol          = "HTTP"
   default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.server.arn
   }
 }
 

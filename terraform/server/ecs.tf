4a54f3b1bfd0ad50b7149f87f435d69538ff6baa
luke-h1 <luke.howsam@yahoo.com>
2023-10-06 21:15:03 +0100

    wip

--- terraform/server/ecs.tf
+++ terraform/server/ecs.tf
@@ -34,7 +34,7 @@ resource "aws_ecs_task_definition" "server" {
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.server_iam_role.arn
+  execution_role_arn       = aws_iam_role.task-execution_role.arn
   task_role_arn            = aws_iam_role.server_iam_role.arn
   container_definitions = jsonencode([
     {
